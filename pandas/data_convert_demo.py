db47d5d44a60858b9340f30cb54ffed0cf5c53d6
francis <francis.xjl@qq.com>
2018-05-25 09:52:45 +0800

    增加数据库以及excel数据转化为pandas对象的演示

--- pandas/data_convert_demo.py
+++ pandas/data_convert_demo.py
@@ -7,7 +7,10 @@
 @time: 2018/5/21 21:56
 @desc:
 """
+import MySQLdb
+import os
 import pandas
+
 
 def list_to_dataframe():
     """
@@ -20,5 +23,30 @@ def list_to_dataframe():
     pd = pandas.DataFrame(list, index=indexs,columns=["first", "second", "third"])
     print(pd)
 
+
+def read_from_mysql():
+    """
+    将mysql表里的数据转化为pandas对象
+    :return:
+    """
+    mysql_con = MySQLdb.connect(host='127.0.0.1', port=3306, user='roomt', passwd='123456', db='easywork', charset="utf8")
+    data = pandas.read_sql('select id,name from room;', con=mysql_con, index_col="id")
+    mysql_con.close()
+    print data
+
+
+def read_from_excel():
+    """
+    将excel里的数据转化为pandas对象
+    :return:
+    """
+    current_folder = os.path.split(os.path.realpath(__file__))[0]
+    data = pandas.read_excel(current_folder + "/view_info.xlsx", index_col="vi_id")
+    return data
+
+
+
 if __name__ == "__main__":
     list_to_dataframe()
+    # read_from_mysql()
+    read_from_excel()
